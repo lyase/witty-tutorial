816de96e35b641a29083523ab5da4114b73a6865
lyase <desafder@debian-witty-prod.bourron.interne.je-resous.fr>
2012-06-12 09:21:34 +0200

    debuging DocRoot cmdl doesn't start app manual start url ok shows docroot=. docs link fails

--- lesson-01-hello-world-in-wt/AdminWindow.cpp
+++ lesson-01-hello-world-in-wt/AdminWindow.cpp
@@ -20,7 +20,9 @@ AdminWindow::AdminWindow(Wt::WContainerWidget* parent) : Wt::WContainerWidget(pa
 
 //#define WRNG __FILE__ "("WRNG_PRIVATE_STR1(__LINE__)"): ------------ : "
   buildinfo+=WRNG ;
-  _debugOutput->setText("debug info "+buildinfo);
+  _debugOutput->setText(Wt::WString("debug info: " + buildinfo + " Docroot: " + Wt::WApplication::instance()->docRoot()));
+  cout <<std::endl<< std::endl <<  " Docroot: " << Wt::WApplication::instance()->docRoot()<<"::"<<std::endl;
+  //_debugOutput->setText("debug info "+buildinfo);
   addWidget(new Wt::WBreak());
   new Wt::WAnchor(Wt::WLink("/doc/html/index.html"), "show docs", this);
 }
