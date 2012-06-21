a52e994651f81c7edc5e0dc9cbaec8181b1a604b
Matthew Sherborne <msherborne@gmail.com>
2012-06-21 16:33:46 +1000

    Trying to make testing work

--- lesson-01-hello-world-in-wt/tests/test_user_is_not_mike.cpp
+++ lesson-01-hello-world-in-wt/tests/test_user_is_not_mike.cpp
@@ -1,4 +1,15 @@
+#include "../User.h"
+#include <string>
+#include <iostream>
 
 int main(int argc, char** argv) {
-    return -1; // Failed test
+    User u;
+    u.setName("John");
+    std::string name = u.getName();
+    if (name == "John")
+        return 0;
+    else {
+        std::cerr << "Expected set name to set the name to 'John' but it set it to " << name << std::endl;
+        return -1; // Failed test
+    }
 }
