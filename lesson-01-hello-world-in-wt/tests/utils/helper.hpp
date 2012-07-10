b3b7deacbc7fd147c8454006fe65bd6bbd61a96d
lyase <desafder@debian-witty-prod.bourron.interne.je-resous.fr>
2012-07-10 08:38:32 +0200

    doing changing path to simulate clicks

--- lesson-01-hello-world-in-wt/tests/utils/helper.hpp
+++ lesson-01-hello-world-in-wt/tests/utils/helper.hpp
@@ -7,8 +7,22 @@
 #include <string>
 #include <boost/test/test_tools.hpp>
 
-namespace help {
-
-template<class Widget>
-Widget* findWidget(Wt::WContainerWidget* parent) {
+#include <Wt/WAnchor>
+#include <Wt/WApplication>
+template <class Widget>
+void click(Widget* widget) {
+widget->click();
+}
+template <>
+void click<Wt::WAnchor>(Wt::WAnchor* widget) {
+const Wt::WLink& link = widget->link();
+switch (link.type()) {
+case Wt::WLink::Url:  // A static URL.
+case Wt::Resource:   // A dynamic resource.
+case Wt::InternalPath: // An internal path.
+                                            {
+Wt::WApplication* app = Wt::WApplication::instance();
+app->setInternalPath(link.internalPath(), true);
+}
+}
 }
