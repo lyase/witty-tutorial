6efe2add6bcac4eaede631364f09aac7f01d3c2e
lyase damasked <lyase.d@gmail.com>
2013-06-23 11:49:34 +1000

    oops, forgot to add calc.hpp

---
+++ lesson-01-hello-world-in-wt/calc.hpp
@@ -0,0 +1,26 @@
+#pragma once
+/** RESTful(ish) API
+ */
+
+#include <Wt/WApplication>
+#include <Wt/WResource>
+#include <Wt/Http/Response>
+
+#include <string>
+
+class Calculator : public Wt::WResource {
+private:
+public:
+    Calculator(const std::string& path, Wt::WObject* parent=nullptr) 
+        : Wt::WResource(parent)
+    {
+        setInternalPath(path);
+        Wt::WApplication::instance()->log("info") << "Resource URL: " << url();
+    }
+    void handleRequest (const Wt::Http::Request&, Wt::Http::Response &response) {
+        Wt::WApplication::instance()->log("info") << "Handle Request Called";
+        response.setStatus(200);
+        response.addHeader("Content-type", "text/plain");
+        response.out() << "Hello\n";
+    }
+};
