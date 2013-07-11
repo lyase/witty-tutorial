5702eb3d49ce4adee3f655c7cec2d937e0029fa2
lyase damasked <lyase.d@gmail.com>
2013-07-11 18:52:26 +1000

    Delete lesson 05

--- lesson-05-auth/Services.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Wt/Auth/AuthService>
-#include <Wt/Auth/PasswordService>
-
-struct Services {
-     Wt::Auth::AuthService auth;
-     Wt::Auth::PasswordService pword;
-     Services();
-};
