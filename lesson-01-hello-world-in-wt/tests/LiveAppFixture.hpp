47bac04761b798cb6c4ec9ce3678029f9df83533
lyase damasked <lyase.d@gmail.com>
2014-07-04 17:40:23 +0200

    removing duplicate header broken build

--- lesson-01-hello-world-in-wt/tests/LiveAppFixture.hpp
+++ lesson-01-hello-world-in-wt/tests/LiveAppFixture.hpp
@@ -1,31 +1,21 @@
 
 #ifndef FIXTURE_H
 #define  FIXTURE_H
-
-
-#include <boost/test/unit_test.hpp>
-#include <boost/test/test_tools.hpp>
-#include <Wt/WWidget>
-#include <Wt/WContainerWidget>
-#include <Wt/Test/WTestEnvironment>
-#include <Wt/WSignal>
-#include <Wt/WAnchor>
-#include "../HelloApp.hpp"
-#include "../MainWindow.hpp"
-
-#include <Wt/Test/WTestEnvironment>
-#include "../HelloApp.hpp"
-#include "../models/User.h"
-#include <Wt/WApplication>
-
-#include <Wt/Test/WTestEnvironment>
-#include "../HelloApp.hpp"
 #include "../Auth/Services.hpp"
 #include "../Auth/Session.hpp"
+#include "../HelloApp.hpp"
+#include "../MainWindow.hpp"
+#include "../models/User.h"
 #include "LiveAppFixture.hpp"
 #include <Wt/Dbo/backend/Sqlite3>
-#include <boost/filesystem.hpp>
 #include <Wt/Test/WTestEnvironment>
+#include <Wt/WAnchor>
+#include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+#include <Wt/WWidget>
+#include <boost/filesystem.hpp>
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test.hpp>
 class LiveAppFixture {
      Wt::Test::WTestEnvironment* env;
      lesson01Auth::Services* services;
