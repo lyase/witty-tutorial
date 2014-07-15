297fb06d071f4cd2d2b4702e056bea86cc5b281b
lyase damasked <lyase.d@gmail.com>
2014-07-15 19:06:38 +0200

    add  topleveldoc target to top level build. add documentation to api class

--- lesson-07-rest-api/ressource/api.hpp
+++ lesson-07-rest-api/ressource/api.hpp
@@ -8,6 +8,26 @@
 #include <functional>
 
 #include <csignal>
+/*! A API   class
+* \details This class is to be used in a witty application it will serve on url following json as string {\"name\": \"Super man\", \"age\": 12}
+* . usage
+*
+*add a single entry point, at the default location (as determined
+*by the server configuration's deploy-path)
+*  \code{.cpp}             API api;
+*       server.setServerConfiguration(argc, argv, WTHTTP_CONFIGURATION);
+*       server.addResource(&api, "/");
+*          if (server.start())
+* \endcode
+* \author Lyase damasked frommattew sherborne initial implementation
+* \version 4.1a
+* \date 1990-2012
+* \pre First initialize the witty server before adding this served ressource .
+* \bug none known ;)
+
+*/
+/*! \brief  this class defines a json ressource
+*/
 class API : public Wt::WResource {
 public:
      API();
