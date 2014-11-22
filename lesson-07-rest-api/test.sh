4ac1f276d2b7e76210e7562eda597378f88f4380
Matthew Sherborne <msherborne@gmail.com>
2014-11-22 22:52:09 +1000

    Lesson 07 now has an SSL server

--- lesson-07-rest-api/test.sh
+++ lesson-07-rest-api/test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./server test_conn --docroot . --http-address 0.0.0.0 --http-port 8000 & 
+./server test_conn --docroot . --http-address 0.0.0.0 --http-port 8000 --ssl-certificate server.crt --ssl-private-key server.key --https-port 8443 --https-address 0.0.0.0 --ssl-tmp-dh dh512.pem &
 # let server start
 sleep 5
 ./clientfromscratch
