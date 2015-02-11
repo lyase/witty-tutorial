8338d461f803768df20c25e33aa243f0115c2588
safder <lyase.d@gmail.com>
2015-02-11 17:54:11 +1100

    ssl requires SSL Private key file ("server.key") must be unreadable for group and others. fixing permission key permission in lesson07 tests all test are ok with ubuntu DISTRIB_DESCRIPTION="Ubuntu 14.04.1 LTS"

--- lesson-07-rest-api/test.sh
+++ lesson-07-rest-api/test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+chmod 600 server.key
 ./server test_conn --docroot . --http-address 0.0.0.0 --http-port 8000 --ssl-certificate server.crt --ssl-private-key server.key --https-port 8443 --https-address 0.0.0.0 --ssl-tmp-dh dh512.pem &
 # let server start
 sleep 5
